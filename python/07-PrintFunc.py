0afb5e36555731c9b65f4e4b596e1b438eb1b3e2
Erick <ahmederick00@gmail.com>
2022-05-23 21:35:21 +0200

    Initial code dump

---
+++ python/07-PrintFunc.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+if n==1:
+    print(1)
+else:
+    for i in range (1, n+1):
+        print(i, end="")
